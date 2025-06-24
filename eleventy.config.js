22022e65824e2952a20a3c48a5f80d9404428d1f
ma9y <214558441+ma9y@users.noreply.github.com>
2025-06-24 12:51:43 +0200

    Initial commit

---
+++ eleventy.config.js
@@ -0,0 +1,17 @@
+export default function (eleventyConfig) {
+    
+    eleventyConfig.addPassthroughCopy("views/assets/img");
+    eleventyConfig.addPassthroughCopy("views/assets/js");
+};
+
+export const config = {
+
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk", 
+
+    dir: {
+        input: "views",
+        layouts: "_layouts",
+        output: "dist"
+    }
+};
---
+++ views/index.md
@@ -0,0 +1,5 @@
+---
+title: runningLog
+layout: base
+---
+# My Eleventy Project
